f3f81824e10d6179e8498757a22fe48e249ccca6
Victor deMatos <58838266+victorkolis@users.noreply.github.com>
2020-07-19 19:11:50 -0300

    Add files via upload

---
+++ AutomatizationSoftwares/phone_number_finder.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Victor deMatos
+# Copyright (c) 2020 CODE FUTURE INVENT
+
+# Description:
+# This piece of software's goal is to satisfy a need to find patterns
+# in a text (of any sort) to trim off phone numeber patterns
+# such as americans and canadians are: xxx-xxx-xxxx
+
+import re
+import pprint
+
+def phone_number_catcher(text_by_user):
+    regex_compiler = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+    list_of_numbers_found = regex_compiler.findall(text_by_user)
+    if list_of_numbers_found == []:
+        print('No number(s) found in the text!')
+    else:
+        pprint.pprint(list_of_numbers_found)
+user_input = input('Enter/paste desired text: ')
+phone_number_catcher(user_input)
